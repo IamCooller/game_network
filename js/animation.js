8ac06937d02550d13fa38dfd01b9b9dc69f4904e
IamCooller <45273146+IamCooller@users.noreply.github.com>
2020-05-08 18:31:52 +0300

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -1,37 +1,42 @@
 
   
 function changeItem_Left() {
-  $('section').toggleClass('magictime rotateLeft'); 
+  $('section').toggleClass('animation-target'); 
   setTimeout(function(){
-  $('section').removeClass('magictime rotateLeft');
-}, 2000);
+  $('section').removeClass('animation-target');
+}, 1000);
 }
 
 function changeItem_Right() {
-  $('section').toggleClass('magictime rotateRight'); 
+  $('section').toggleClass('animation-target_two'); 
   setTimeout(function(){
-  $('section').removeClass('magictime rotateRight');
-}, 2000);
+  $('section').removeClass('animation-target_two');
+}, 1000);
 }
 
 
 function changeItem_Star() {
-  $('.Start_Game').toggleClass('magictime tinDownOut'); 
+
+ 
   setTimeout(function(){
-    $('.Start_Game').css('display', 'none'); 
-}, 1000);  
-setTimeout(function(){
-$('.Start_Game-Two').toggleClass('magictime tinDownIn'); 
-$('.Start_Game-Two').css('display', '');
+    $('.Start_Game').css('display', 'none');      
 }, 1500);
+
+setTimeout(function(){
+$('.Start_Game-Two').css('display', '');
+$('.Start_Game-Two').toggleClass('magictime tinDownIn'); 
+$('.Start_Game').css('position', 'absolute');
+$('.Start_Game').css('z-index', '-1');
+}, 1000);
+
 setTimeout(function(){
   $('.Start_Game-Two').removeClass('magictime tinDownIn'); 
-  }, 2500);
+  }, 1500);
 }
 
 
 function changeItem_Left_CardsTwo() {
-  $('.Start_Game-Two').toggleClass('magictime rotateLeft'); 
+  $('.Start_Game-Two').toggleClass('animation-target'); 
   setTimeout(function(){
     $('.Start_Game-Two').css('display', 'none'); 
 }, 1000); 
