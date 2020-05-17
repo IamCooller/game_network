fc5fc87b36fefff8337c59e18f48c83a7cc141d1
IamCooller <45273146+IamCooller@users.noreply.github.com>
2020-05-17 19:11:04 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -8,26 +8,33 @@ new Vue({
         mounthly:0,
         peoples:0,
         balance: 20000,
+        change: 0,
+        change_peoples: 0,
         LastAction:0,
-        ButtonsLeft: false,
-        ButtonsRight: false,
+        Buttons:true,
         none: false,
         Next: true,
         cards:[
             {
             "id": 1,
             "condition": "Ваш друг предложил вам сходить на презентацию компании, пойдете?",
-            "ButtonsLeft": false
+            "Buttons": false
             },
             {
             "id": 2,
             "condition": "Сетевой бизнес - это когда ты строишь маленькую экономику на рельном товаре. Вы готовы стартануть прямо сейчас? ",
-            "ButtonsLeft": false,
+            "Buttons": false,
             "false": "Это действие стоит - 10000",
+           
             },
             {
             "id": 3,
              "condition": "Нужно уделить 5 часов изучение материалов. Изучить или сразу в бой?",
+             "change": 10000,
+             "action": 0,
+             "LastAction": -10000,
+             "change_peoples": 1,
+            "action_peoples": 1,
             },    
             {
             "id": 4,
@@ -37,18 +44,31 @@ new Vue({
             {
                 "id": 5,
                 "condition": "Вы нашли 15 человек, но многие из них отказались вас слушать, однако, вам удалось убедить 2 людей присоедениться",
-                "done": "2 человека в структуру и +10000 рублей на баланс"
+                "done": "2 человека в структуру и +10000 рублей на баланс",
+                "change": 10000,
+                "action": 1,
+                "Buttons": false,
+                "LastAction": "+10000",
+                "change_peoples": 2,
+                "action_peoples": 1,
             },
             {
                 "id":6,
-                "condition": "Ничего не произошло"
+                "condition": "Пройти обучение у лидера",
                 
             },
             {
                 "id":7,
-                "condition": ""
-                
-            }                            
+                "condition": "Создать страницы в социальных сетях",
+            },
+            {
+                "id":8,
+                "condition": "Пройти вебинар, который организует компания. Обучение займет много времени",
+            },
+            {
+                "id":9,
+                "condition": "Распечатать свои визитки. Будет стоить 1000 рублей",
+            },
         ]
 
     },
@@ -60,18 +80,56 @@ new Vue({
         },
         round_left:function(){
             this.cards.shift();
+            this.check_round_left();
             this.check_balance();
-           this.check_round_left();
+            this.balance += this.peoples * 2000;
             console.log(this.cards[0].id);
         },
         round_right:function (){
             this.cards.shift();
             this.check_balance();
             this.check_round_right();
+            this.balance += this.peoples * 2000;
             console.log(this.cards[0].id);
         },
         check_balance:function(){
-            if(this.cards[0].id == 3){
+            switch(this.cards[0].action){
+                case 0:
+                this.balance -= this.cards[0].change;
+                this.LastAction = 0 - this.cards[0].change;
+                break;
+                case 1:
+                this.balance += this.cards[0].change;
+                this.LastAction = 0 + this.cards[0].change;
+                break;
+                default:
+                this.balance += 0;
+            }
+            switch(this.cards[0].Buttons){
+                case false:
+                this.none = true;
+                this.ButtonsLeft= false;
+                this.ButtonsRight= false;
+                this.Next= true;
+                break;
+                default:
+                this.ButtonsLeft= true;
+                this.ButtonsRight= true;
+                this.Next= false;
+            }
+            switch(this.cards[0].action_peoples){
+                case 0:
+                this.peoples -= this.cards[0].change_peoples;
+                break;
+                case 1:
+                this.peoples += this.cards[0].change_peoples;
+                break;
+                default:
+                this.peoples +=0;
+            }
+  
+         
+           /*  if(this.cards[0].id == 3){
             this.balance -= 10000;
             this.LastAction -= 10000;
             this.Next= false;
@@ -91,13 +149,14 @@ new Vue({
                 this.ButtonsLeft= true;
                 this.ButtonsRight= true;
                 this.Next= false;
-            }
+            } */
     },
     check_round_left:function(){
         if(this.cards[0].id == 5)
         {
             this.cards.shift();
-            this.cards[0].id == 7;
+            this.cards[0].condition="Ничего не произошло";
+            this.cards[0].Buttons= false;
             console.log(true);
         }
     },
